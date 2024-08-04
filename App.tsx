7648d4c7d4728d27f217959d38d4378c4690bbe0
DayOfGame <122552857+DayOfGame@users.noreply.github.com>
2024-08-04 10:58:42 +0300

    add readability

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Escrow from './Escrow'; 
+import Escrow from './Escrow';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={() => <div>Main Page</div>} />
+        <Route exact path="/" component={MainPage} />
         <Route path="/escrow" component={Escrow} />
-        <Route path="*" component={() => <div>404 Not Found</div>} />
+        <Route path="*" component={NotFoundPage} />
       </Switch>
     </Router>
   );
 };
+
+const MainPage: React.FC = () => (
+  <div>Main Page</div>
+);
+
+const NotFoundPage: React.FC = () => (
+  <div>404 Not Found</div>
+);
 
 export default App;
