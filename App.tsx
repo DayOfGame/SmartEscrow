586dfb04f0465b27e9032a14d949381e7e991888
DayOfGame <122552857+DayOfGame@users.noreply.github.com>
2024-08-05 15:14:17 +0300

    implemented function

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Escrow from './Escrow';
 
+const EscrowDetail: React.FC = () => (
+  <div>Escrow Contract Details</div>
+);
+
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={MainPage} />
-        <Route path="/escrow" component={Escrow} />
+        <Route exact path="/escrow" component={Escrow} />
+        <Route path="/escrow/:id" component={EscrowDetail} />
         <Route path="*" component={NotFoundPage} />
       </Switch>
     </Router>
