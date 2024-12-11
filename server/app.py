41ae1fd39af578f51783b5a2cc390a8cfa336f42
DayOfGame <122552857+DayOfGame@users.noreply.github.com>
2024-12-11 15:50:46 +0200

    Initialize app.py

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+if __name__ == '__main__':
+    port = os.getenv('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
