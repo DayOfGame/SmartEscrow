058f8aeab1731ed97925027dbbe78cd64a186976
DayOfGame <122552857+DayOfGame@users.noreply.github.com>
2025-04-10 17:19:03 +0300

    change variable

--- server/routes.py
+++ server/routes.py
@@ -6,21 +6,21 @@ load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+ENV_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 @app.route('/')
-def home():
-    return jsonify({"message": "Welcome to our API!"})
+def welcome():
+    return jsonify({"message": "Welcome to our SmartEscrow API!"})
 
 @app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {"data": "This could be data from a database or any source."}
-    return jsonify(data)
+def fetch_data():
+    example_data = {"data": "This could be data fetched from a database or another source."}
+    return jsonify(example_data)
 
 @app.route('/api/data', methods=['POST'])
-def post_data():
-    data = request.json
-    return jsonify({"message": "Data received", "yourData": data}), 201
+def receive_data():
+    submitted_data = request.json
+    return jsonify({"message": "Data successfully received", "receivedData": submitted_data}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
